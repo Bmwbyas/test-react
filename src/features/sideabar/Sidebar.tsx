ab92b977f3a91e61200e8e633ac898dffd974f45
alexAskerko <askerko50@gmail.com>
2023-03-03 23:00:08 +0300

    feat(create navbar. Styled this component)

--- src/features/sideabar/Sidebar.tsx
+++ src/features/sideabar/Sidebar.tsx
@@ -1,33 +1,24 @@
-import React from 'react';
-import {
-    Box,
-    Button,
-    Divider,
-    Drawer,
-    Grid,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText, Stack, Typography
-} from "@mui/material";
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import React, {useState} from 'react';
+import {Divider, Grid, Link, Stack, Typography} from "@mui/material";
 import {variables} from "../../assets/styled/variables";
+import {v1} from "uuid";
 
 const menuItems = [{
+    id: v1(),
     name: "blabla",
     createDate: '30.01.202020',
     text: 'tilitili tralbasdff'
 
 },
     {
+        id: v1(),
         name: "bbbb",
         createDate: '30.01.202022220',
         text: 'fdsffdssf trafdsfds lbasdff'
 
     },
     {
+        id: v1(),
         name: "aaaa",
         createDate: '30.03.202020',
         text: 'Lorem  vfdvjsorkl; vlsdfv;lkfv '
@@ -35,6 +26,8 @@ const menuItems = [{
     }]
 
 export const Sidebar = () => {
+    const [activeNote, setActiveNote] = useState<null | string>(null)
+
     return (
         <Grid item xs={3}
               sx={{
@@ -46,25 +39,37 @@ export const Sidebar = () => {
         >
             <Typography variant="h6" sx={{pl: 4}}> Сегодня </Typography>
             <Divider sx={{bgcolor: variables.activeColor, mt: 1, mb: 2}}/>
-            <Stack  spacing={2} width={'90%'} mx={"auto"}>
+            <Stack spacing={2} width={'90%'} mx={"auto"}>
                 {menuItems.map((el, index) => {
-                    return <Stack
-                        direction={"column"}
-                        sx={{bgcolor: variables.selectColor, borderRadius: 2, p: 1,}}
-                        key={index}
-                    >
-                        <Typography sx={{
+                    const activeColor = activeNote === el.id ? variables.selectColor : ''
+                    const activeNoteHandler=()=>{
+                        setActiveNote(el.id)
+                    }
 
-                            width: '100%',
-                            fontWeight: 700,
-                            color: "white"
-                        }}>{el.name}</Typography>
-                        <Stack direction={"row"} spacing={1}>
-                            <Typography sx={{fontWeight: 700}}> {el.createDate.slice(0, 5)} </Typography>
-                            <Typography>{el.text}</Typography>
+                    return <Link href="#" onClick={activeNoteHandler} color="inherit" underline="none"  >
+                        <Stack
+                            direction={"column"}
+                            sx={{
+                                bgcolor: activeColor,
+                                borderRadius: 2,
+                                p: 1,
+                                borderBottom: `1px solid ${variables.selectColor}`
+                            }}
+                            key={index}
+                        >
+                            <Typography sx={{
+
+                                width: '100%',
+                                fontWeight: 700,
+                                color: "white"
+                            }}>{el.name}</Typography>
+                            <Stack direction={"row"} spacing={1}>
+                                <Typography sx={{fontWeight: 700}}> {el.createDate.slice(0, 5)} </Typography>
+                                <Typography>{el.text}</Typography>
+                            </Stack>
+
                         </Stack>
-                        
-                    </Stack>
+                    </Link>
                 })}
             </Stack>
         </Grid>
